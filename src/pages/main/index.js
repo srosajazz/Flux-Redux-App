5f6720bdf27739731f8b6c2d0e11ada17e28e8be
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-04 18:10:18 -0400

    Application interface

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
-const Main = () => <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.render.handleAddRepository}>
+          <input
+            placeholder="user/repository"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Add</button>
+
+          <ul>
+            <li>
+              <p>
+                <strong>facebook/react</strong> (A declarative, efficient, and flexible JavaScript
+                library for building user interfaces.)
+              </p>
+              <a href="https://github.com/facebook/react">Access Github</a>
+            </li>
+          </ul>
+        </form>
+      </Fragment>
+    );
+  }
+}
