756cf08cd45184d3697ff5d307fb6ffd07533ff5
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-04 15:27:49 -0400

    Reactotron

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import './config/ReacotronConfig';
 import { Provider } from 'react-redux';
 import store from './store';
 import TodoList from './TodoList';
+
+console.tron.log('Hello Sergio');
 
 const App = () => (
   <Provider store={store}>
